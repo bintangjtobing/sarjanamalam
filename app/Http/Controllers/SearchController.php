d3dea4153bf93932dfde5fe301f715306c2dc662
Star CodeX <hello@bintangtobing.com>
2020-01-18 13:29:28 +0700

    update

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\messagesDB;
-use App\UserMod;
 use App\Thread;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -23,22 +21,6 @@ class SearchController extends Controller
             ->where('thread', 'like', "%" . $get_search . "%")
             ->select('threads.*')
             ->paginate(15);
-        dd($search);
-        // return view('homepage.results_search');
-    }
-
-    // CONTROLLER SEARCH ENGINE
-    public function processcari(Request $request)
-    {
-        // Menangkap data cari
-        $get_search = $request->searchinput;
-
-        // Ambil data  dari table threads
-        $search = DB::table('threads')
-            ->where('thread', 'like', "%" . $get_search . "%")
-            ->select('threads.*')
-            ->paginate(15);
-
         dd($search);
     }
 }

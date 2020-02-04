e4a9b8835fbced9ed0bf5c93cea273a4044d4c72
Star CodeX <hello@bintangtobing.com>
2020-02-04 12:08:13 +0700

    update

--- resources/views/homepage/welcomenew.blade.php
+++ resources/views/homepage/welcomenew.blade.php
@@ -15,21 +15,6 @@
         gtag('config', 'UA-68593210-11');
 
     </script>
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-68593210-11"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'UA-68593210-11');
-
-    </script>
-
     <!-- Google Tag Manager -->
     <script>
         (function (w, d, s, l, i) {

c30156fe5ee70e40a46e276cb3568f243eb887c0
Star CodeX <hello@bintangtobing.com>
2020-02-06 19:09:59 +0700

    update

--- resources/views/homepage/results_search.blade.php
+++ resources/views/homepage/results_search.blade.php
@@ -441,7 +441,7 @@
                             <!-- Pagination -->
                             <nav class="d-flex justify-content-center">
                                 <ul class="pagination pagination-sm   ">
-                                    <li class="page-item disabled">
+                                    {{-- <li class="page-item disabled">
                                         <a class="page-link" href="#" aria-label="Previous">
                                             <span aria-hidden="true">&laquo;</span>
                                             <span class="sr-only">Previous</span>
@@ -457,7 +457,8 @@
                                             <span aria-hidden="true">&raquo;</span>
                                             <span class="sr-only">Next</span>
                                         </a>
-                                    </li>
+                                    </li> --}}
+                                    {{$search->links()}}
                                     {{-- {{$search->links()}} --}}
                                 </ul>
                             </nav>

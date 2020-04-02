0389576c970bf87992d3d63b55ad69dce707efc0
Star CodeX <hello@bintangtobing.com>
2020-04-02 11:25:36 +0700

    update

--- resources/views/forum/messages/home.blade.php
+++ resources/views/forum/messages/home.blade.php
@@ -24,7 +24,7 @@
                                     <span title="Verified" style="
     position: relative;
     margin-left: -15px;
-    margin-top: 25px; background-color: #fff; border-radius: 50%; top: 5px;
+    margin-top: 15px; background-color: #fff; border-radius: 50%; top: 5px;
     right: 13px;"><img src="https://res.cloudinary.com/sarjanamalam/image/upload/v1584348883/based/checkmark_ty9wnj.svg"
                                             alt="Verified" style="width:15px !important; height:15px !important;
                                             margin-left:2px;position:relative;"></span>
